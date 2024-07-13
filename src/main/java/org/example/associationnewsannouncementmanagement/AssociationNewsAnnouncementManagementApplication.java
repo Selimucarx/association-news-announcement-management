f603ae4949966a7c14e39c50530054be56baa434
Selim Ucar <selim.ucarx@gmail.com>
2024-07-14 02:59:38 +0300

    First Commit :)

---
+++ src/main/java/org/example/associationnewsannouncementmanagement/AssociationNewsAnnouncementManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.associationnewsannouncementmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociationNewsAnnouncementManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociationNewsAnnouncementManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/associationnewsannouncementmanagement/AssociationNewsAnnouncementManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.associationnewsannouncementmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociationNewsAnnouncementManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
